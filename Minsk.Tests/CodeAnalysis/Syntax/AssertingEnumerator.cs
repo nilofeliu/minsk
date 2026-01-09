fd49f77e5a8067785d5dedf8fe6cb293f57b3e6d
Nilo Martinez Jr <nilofeliu@hotmail.com>
2026-01-09 12:32:56 -0300

    Add tests o verify binary operator precedent are honored

---
+++ Minsk.Tests/CodeAnalysis/Syntax/AssertingEnumerator.cs
@@ -0,0 +1,74 @@
+﻿using Minsk.CodeAnalysis.Syntax;
+
+namespace Minsk.Tests.CodeAnalysis.Syntax;
+
+internal sealed class AssertingEnumerator : IDisposable
+{
+    private readonly IEnumerator<SyntaxNode> _enumerator;
+
+    private bool _hasErrors;
+
+    public AssertingEnumerator(SyntaxNode node)
+    {
+            _enumerator = Flatten(node).GetEnumerator();
+    }
+
+    private bool MarkFailed()
+    {
+        _hasErrors = true;
+        return false;
+    }
+
+    private IEnumerable<SyntaxNode> Flatten(SyntaxNode node)
+    {
+        var stack = new Stack<SyntaxNode>();
+        stack.Push(node);
+
+        while (stack.Count > 0)
+        {
+            var n = stack.Pop();
+            yield return n;
+
+            foreach (var child in n.GetChildren().Reverse())
+            {
+                stack.Push(child);
+            }
+        }
+    }
+
+    public void AssertToken(SyntaxKind kind, string text)
+    {
+        try
+        {
+            Assert.True(_enumerator.MoveNext());
+            var token = Assert.IsType<SyntaxToken>(_enumerator.Current);
+            Assert.Equal(kind, token.Kind);
+            Assert.Equal(text, token.Text);
+        }
+        catch when (MarkFailed())
+        {
+            throw;
+        }
+    }
+    public void AssertNode(SyntaxKind kind)
+    {
+        try
+        {
+            Assert.True(_enumerator.MoveNext());
+            Assert.Equal(kind, _enumerator.Current.Kind);
+
+            Assert.IsNotType<SyntaxToken>(_enumerator.Current);            
+        }
+        catch when (MarkFailed())
+        {
+                throw;
+        }
+    }
+
+    public void Dispose()
+    {
+        if (!_hasErrors)
+        { Assert.False(_enumerator.MoveNext()); }
+        _enumerator.Dispose();
+    }
+}
