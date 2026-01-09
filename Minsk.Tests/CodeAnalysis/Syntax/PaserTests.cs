d6a94268fe83157b2f23605d78cd62d1a3565c5d
Nilo Martinez Jr <nilofeliu@hotmail.com>
2026-01-09 12:30:45 -0300

    Update PaserTests.cs

--- Minsk.Tests/CodeAnalysis/Syntax/PaserTests.cs
+++ Minsk.Tests/CodeAnalysis/Syntax/PaserTests.cs
@@ -4,9 +4,61 @@ namespace Minsk.Tests.CodeAnalysis.Syntax;
 
 public partial class PaserTests
 {
-    //[Theory]
-    //public void Parser_BinaryExpression_HonorPrecedences(SyntaxKind op1, SyntaxKind op2)
-    //{
+    [Theory]
+    [MemberData(nameof(GetBinaryOperatorPairsData))]
+    public void Parser_BinaryExpression_HonorPrecedences(SyntaxKind op1, SyntaxKind op2)
+    {
+        var op1Precedence = SyntaxFacts.GetBinaryOperatorPrecedence(op1);
+        var op2Precedence = SyntaxFacts.GetBinaryOperatorPrecedence(op2);
+        var op1Text = SyntaxFacts.GetText(op1);
+        var op2Text = SyntaxFacts.GetText(op2);
+        var text = $"a {op1Text} b {op2Text} c";
+        var expression = SyntaxTree.Parse(text).Root;
 
-    //}
+        if (op1Precedence >= op2Precedence)
+        {
+            using (var e = new AssertingEnumerator(expression))
+            {
+                e.AssertNode(SyntaxKind.BinaryExpression);
+                e.AssertNode(SyntaxKind.BinaryExpression);
+                e.AssertNode(SyntaxKind.NameExpression);
+                e.AssertToken(SyntaxKind.IdentifierToken, "a");
+                e.AssertToken(op1, op1Text);
+                e.AssertNode(SyntaxKind.NameExpression);
+                e.AssertToken(SyntaxKind.IdentifierToken, "b");
+                e.AssertToken(op2, op2Text);
+                e.AssertNode(SyntaxKind.NameExpression);
+                e.AssertToken(SyntaxKind.IdentifierToken, "c");
+            }
+        }
+        else
+        {
+            using (var e = new AssertingEnumerator(expression))
+            {
+                e.AssertNode(SyntaxKind.BinaryExpression);
+                e.AssertNode(SyntaxKind.NameExpression);
+                e.AssertToken(SyntaxKind.IdentifierToken, "a");
+                e.AssertToken(op1, op1Text);
+                e.AssertNode(SyntaxKind.BinaryExpression);
+                e.AssertNode(SyntaxKind.NameExpression);
+                e.AssertToken(SyntaxKind.IdentifierToken, "b");
+                e.AssertToken(op2, op2Text);
+                e.AssertNode(SyntaxKind.NameExpression);
+                e.AssertToken(SyntaxKind.IdentifierToken, "c");                           }
+        }
+    }
+
+    public static IEnumerable<object[]> GetBinaryOperatorPairsData()
+    {
+        foreach (var op1 in SyntaxFacts.GetBinaryOperatorKinds())
+        {
+            foreach (var op2 in SyntaxFacts.GetBinaryOperatorKinds())
+            {
+                var op1Precedence = SyntaxFacts.GetBinaryOperatorPrecedence(op1);
+                var op2Precedence = SyntaxFacts.GetBinaryOperatorPrecedence(op2);
+                if (op1Precedence >= op2Precedence)
+                    yield return new object[] { op1, op2 };
+            }
+        }
+    }
 }
