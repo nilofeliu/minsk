06a5cb085481023fcf1eb05a1c6c3ae115987702
Nilo Martinez Jr <nilofeliu@hotmail.com>
2026-02-03 08:09:44 -0300

    Add tests for While and For Statements

--- Minsk.Tests/CodeAnalysis/EvaluatorTests.cs
+++ Minsk.Tests/CodeAnalysis/EvaluatorTests.cs
@@ -52,6 +52,8 @@ namespace Minsk.Tests.CodeAnalysis
 
         [InlineData("{var i = 10 var result = 0 while i > 0 { result = result + i i = i -1} result }", 55 )]
 
+        [InlineData("{var result = 0 for i = 1 to 10 { result = result + i } result }", 55)]
+
         public void SyntaxFact_GetText_RoundTrips(string text, object expectedValue)
         {
             var syntaxTree = SyntaxTree.Parse(text);
@@ -101,6 +103,63 @@ namespace Minsk.Tests.CodeAnalysis
 
             AssertDiagnostics(text, diagnostics);
         }
+
+        [Fact]
+        public void Evaluator_WhileStatement_Reports_CannotConvert()
+        {
+            var text = @"
+                {
+                    var x = 0
+                    while [10]
+                        x = 10
+                }
+            ";
+
+            var diagnostics = @"
+                Cannot convert type 'System.Int32' to 'System.Boolean'.
+            ";
+
+            AssertDiagnostics(text, diagnostics);
+        }
+
+        [Fact]
+        public void Evaluator_ForStatement_Reports_CannotConvert_LowerBound()
+        {
+            var text = @"
+                {
+                    var result = 0
+                    for i = [false] to 10
+                        result = result + i
+                }
+            ";
+
+            var diagnostics = @"
+                Cannot convert type 'System.Boolean' to 'System.Int32'.
+            ";
+
+            AssertDiagnostics(text, diagnostics);
+        }
+
+        [Fact]
+        public void Evaluator_ForStatement_Reports_CannotConvert_UpperBound()
+        {
+            var text = @"
+                {
+                    var result = 0
+                    for i = 1 to [true]
+                        result = result + i
+                }
+            ";
+
+            var diagnostics = @"
+                Cannot convert type 'System.Boolean' to 'System.Int32'.
+            ";
+
+            AssertDiagnostics(text, diagnostics);
+        }
+
+
+        [Fact]
         public void Evaluator_NameExpression_Reports_Undefined()
         {
             var text = @"[x] * 10";
