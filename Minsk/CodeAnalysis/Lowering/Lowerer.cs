8065c5a8c839c51784ddfbb77222f62da8da15f5
Nilo Martinez Jr <nilofeliu@hotmail.com>
2026-02-05 21:38:24 -0300

    Lower for-statements into while-statements

--- Minsk/CodeAnalysis/Lowering/Lowerer.cs
+++ Minsk/CodeAnalysis/Lowering/Lowerer.cs
@@ -1,6 +1,8 @@
 ﻿using Minsk.CodeAnalysis.Binding;
+using Minsk.CodeAnalysis.Syntax;
 using System;
 using System.Collections.Generic;
+using System.Collections.Immutable;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,5 +22,45 @@ namespace Minsk.CodeAnalysis.Lowering
             return lowerer.RewriteStatement(statement);
         }
 
+        protected override BoundStatement RewriteForStatement(BoundForStatement node)
+        {
+            // for <var> = <lower> to <upper>
+            //      <body>
+            //
+            // ---->
+            //
+            // {
+            //      var <var> = <lower>
+            //      while (<var> <= <upper>)
+            //      {
+            //          <body>
+            //          <var> = <var> + 1
+            //      }   
+            // }
+
+            var variableDeclaration = new BoundVariableDeclaration(node.Variable, node.LowerBound);
+            var variableExpression = new BoundVariableExpression(node.Variable);
+            var condition = new BoundBinaryExpression(
+                variableExpression,
+                BoundBinaryOperator.Bind(SyntaxKind.LessOrEqualsToken, typeof(int), typeof(int)),
+                node.UpperBound
+            );
+            var increment = new BoundExpressionStatement(
+                new BoundAssignmentExpression(
+                    node.Variable,
+                    new BoundBinaryExpression(
+                            variableExpression,
+                            BoundBinaryOperator.Bind(SyntaxKind.PlusToken, typeof(int), typeof(int)),
+                            new BoundLiteralExpression(1)
+                    )
+                )
+            );
+            var whileBody = new BoundBlockStatement(ImmutableArray.Create<BoundStatement>(node.Body, increment));
+            var whileStatement = new BoundWhileStatement(condition, whileBody);
+            var result = new BoundBlockStatement(ImmutableArray.Create<BoundStatement>(variableDeclaration, whileStatement));
+
+            return RewriteStatement(result);
+        }
     }
 }
+
