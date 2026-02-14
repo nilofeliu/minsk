eaf263702e739a7bff8cef6f3db3ed5acca7e3d4
Nilo Martinez Jr <nilofeliu@hotmail.com>
2026-02-14 00:25:22 -0300

    Minor Adjustments to SwitchStatementSyntax

--- Minsk/CodeAnalysis/Syntax/Statement/IfStatementSyntax.cs
+++ Minsk/CodeAnalysis/Syntax/Statement/IfStatementSyntax.cs
@@ -1,23 +1,32 @@
-﻿using Minsk.CodeAnalysis.Syntax.Expression;
+﻿using Minsk.CodeAnalysis.Symbols;
+using Minsk.CodeAnalysis.Syntax.Core;
+using Minsk.CodeAnalysis.Syntax.Expression;
 using Minsk.CodeAnalysis.Syntax.Kind;
 using Minsk.CodeAnalysis.Syntax.Object;
+using System.Collections.Generic;
+using System.Collections.Immutable;
 
-namespace Minsk.CodeAnalysis.Syntax.Statement
+namespace Minsk.CodeAnalysis.Syntax.Statement;
+
+
+
+public sealed class IfStatementSyntax : StatementSyntax
 {
-    public sealed class IfStatementSyntax : StatementSyntax
+    public IfStatementSyntax(SyntaxToken ifKeyword,
+        ExpressionSyntax condition,
+        StatementSyntax thenStatement,
+        ElseClauseSyntax elseClause)
     {
-        public IfStatementSyntax(SyntaxToken ifKeyword, ExpressionSyntax condition, StatementSyntax thenStatement, ElseClauseSyntax elseClause)
-        {
-            IfKeyword = ifKeyword;
-            Condition = condition;
-            ThenStatement = thenStatement;
-            ElseClause = elseClause;
-        }
-
-        public override SyntaxKind Kind => SyntaxKind.IfStatement;
-        public SyntaxToken IfKeyword { get; }
-        public ExpressionSyntax Condition { get; }
-        public StatementSyntax ThenStatement { get; }
-        public ElseClauseSyntax ElseClause { get; }
+        IfKeyword = ifKeyword;
+        Condition = condition;
+        ThenStatement = thenStatement;
+        ElseClause = elseClause;
     }
+
+    public override SyntaxKind Kind => SyntaxKind.IfStatement;
+    public SyntaxToken IfKeyword { get; }
+    public ExpressionSyntax Condition { get; }
+    public StatementSyntax ThenStatement { get; }
+    public ElseClauseSyntax ElseClause { get; }
 }
+       
